ef8975235599a74c8a6eeef94b59ecbbc694b3a7
unknown <clecio702@yahoo.com.br>
2020-08-29 22:01:46 -0300

    subindo UserInfo

---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  Container,
+  Profile,
+  Avatar,
+  UserData,
+  Icons,
+  MicIcon,
+  HeadphoneIcon,
+  SettingsIcon
+} from './styles';
+
+const UserInfo: React.FC = () => {
+  return <Container>
+    <Profile>
+      <Avatar />
+      <UserData>
+        <strong>Pedrx 777</strong>
+        <span>#2483</span>
+      </UserData>
+    </Profile>
+
+    <Icons>
+      <MicIcon />
+      <HeadphoneIcon />
+      <SettingsIcon />
+    </Icons>
+  </Container>;
+};
+
+export default UserInfo;
